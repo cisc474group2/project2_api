c761b56b51d741a152d9d8a6c2495fa81ad0cb43
Mark Wolgin <wolginmark@gmail.com>
2020-10-29 14:26:47 -0400

    Nope

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,5 @@ export const Config={
     serverport: process.env.PORT || 3000,
     secret: process.env.SECRET || "some-secret-goes-here",
     tokenLife: 1800,
-    url: process.env.MONGOURL || "mongodb://localhost:27017/"    
+    url: process.env.MONGOURL || "mongodb+srv://short:fuse@devcluster.8ejzl.mongodb.net/<dbname>?retryWrites=true&w=majority"    
 };
