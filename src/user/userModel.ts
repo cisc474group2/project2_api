1ee2c71e35c63404cef4eb1a0ed0f8b44d478f4c
Sarah Joyce <sjoyce@udel.edu>
2020-10-29 13:20:16 -0400

    setup user routes

--- src/user/userModel.ts
+++ src/user/userModel.ts
@@ -1,24 +1,27 @@
 
-export class ProjectsModel{
-    id='';
-    name='';
-    description?='';
-    groupid='';
-    groupMembers:string[]=[];
-    semester='';
-    projectNumber=0;
+export class UserModel{
+    user_id='';
+    email='';
+    password='';
+    password_reset='';
+    type='';
+    type_obj='';
+    last_login='';
+    reg_events:string[]=[];
 
-    static fromObject(object:any):ProjectsModel{
-        const p:ProjectsModel=new ProjectsModel();
-        p.name=object.name;
-        p.description=object.description;
-        p.groupid=object.groupId;
-        p.groupMembers=object.groupMembers;
-        p.semester=object.semester
-        p.projectNumber=object.projectNumber;
-        return p;
+    static fromObject(object:any):UserModel{
+        const u:UserModel=new UserModel();
+        u.user_id=object.user_id;
+        u.email=object.email;
+        u.password=object.password;
+        u.password_reset=object.password_reset;
+        u.type=object.type;
+        u.type_obj=object.type_object;
+        u.last_login=object.last_login;
+        u.reg_events=object.reg_events;
+        return u;
     }
     toObject():any{
-        return {name:this.name,description:this.description,groupid:this.groupid,groupMembers:this.groupMembers,semester:this.semester,projectNumber:this.projectNumber};
+        return {user_id:this.user_id,email:this.email,password:this.password,password_reset:this.password_reset,type:this.type,type_obj:this.type_obj,last_login:this.last_login,reg_events:this.reg_events};
     }
 }
