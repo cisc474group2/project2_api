8fd138f6b71a8aaf4e454ceba27338a06aa4455e
Mark Wolgin <wolginmark@gmail.com>
2020-10-29 13:34:27 -0400

    added Routse

--- src/user/bus/busRouter.ts
+++ src/user/bus/busRouter.ts
@@ -1,13 +1,17 @@
 import express, { RequestHandler } from 'express';
 import { AppRouter } from "../../common/AppRouter";
+import { SecurityMiddleware } from "../../security/securityMiddleware";
+import { BusController } from './busController';
 
 //This is just an example second router to show how additional routers can be added
-export class BusModel extends AppRouter{
-    static projController: BusModel=new BusModel();
+export class BusRouter extends AppRouter{
+    static BusController: BusController=new BusController();
     constructor(){super();}
 
     //sets up the routes within this module shows an example of a route that requires authorization, and one that does not
     setupRoutes(): void {      
-        //
+        this.expressRouter.post('bus/',[SecurityMiddleware.RequireAuth],BusRouter.BusController.getAllBusinesses);
+        this.expressRouter.put('bus/:id',[SecurityMiddleware.RequireAuth],BusRouter.BusController.getBusiness);
+        this.expressRouter.delete('bus/:id/hosted_events',[SecurityMiddleware.RequireAuth],BusRouter.BusController.getHostedEvents);
     }    
 }
--- src/user/userRouter.ts
+++ src/user/userRouter.ts
@@ -1,6 +1,8 @@
 import { AppRouter } from "../common/AppRouter";
 import { SecurityMiddleware } from "../security/securityMiddleware";
 import { UserController } from "./userController";
+import { BusRouter } from './bus/busRouter';
+//import { IndRouter } from './bus/indRouter';
 
 //This is just an example second router to show how additional routers can be added
 export class UserRouter extends AppRouter{
@@ -9,6 +11,9 @@ export class UserRouter extends AppRouter{
 
     //sets up the routes within this module shows an example of a route that requires authorization, and one that does not
     setupRoutes(): void {      
+        this.addRouter('/bus', new BusRouter());
+        //this.addRouter('/ind', new IndRouter());
+
         this.expressRouter.post('user/',[SecurityMiddleware.RequireAuth],UserRouter.userController.createUser);
         this.expressRouter.put('user/:id',[SecurityMiddleware.RequireAuth],UserRouter.userController.updateUser);
         this.expressRouter.delete('user/:id',[SecurityMiddleware.RequireAuth],UserRouter.userController.deleteUser);
