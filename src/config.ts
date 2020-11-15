f12b97e9a09a528b6c564e39ee54d1bd53f31945
Mark Wolgin <wolginmark@gmail.com>
2020-11-15 16:46:48 -0500

    config

--- src/config.ts
+++
@@ -1,8 +0,0 @@
-//configuration information 
-export const Config={
-    serverport: process.env.PORT || 3000,
-    secret: process.env.SECRET || "some-secret-goes-here",
-    tokenLife: 1800,
-    url: process.env.MONGOURL || "mongodb+srv://short:fuse@devcluster.8ejzl.mongodb.net?retryWrites=true&w=majority",
-    url_elevated: process.env.MONGOURL || "mongodb+srv://elevated:p3rm1ss10ns@devcluster.8ejzl.mongodb.net?retryWrites=true&w=majority"
-};
